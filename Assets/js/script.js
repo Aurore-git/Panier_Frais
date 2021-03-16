5ffc2cb3932b5d0c19c43034874c6c3a9024a407
Aurore-git <aurore.malochet@gmail.com>
2021-03-16 14:01:07 +0100

    css / js / font / images du site

---
+++ Assets/js/script.js
@@ -0,0 +1,81 @@
+/*Variables*/
+
+var rowConfiture= document.getElementById('row-confitures');
+var confitureNone = document.getElementById('confitures');
+
+var rowBoisson= document.getElementById('row-boissons');
+var boissonNone = document.getElementById('boissons');
+
+var rowFruit= document.getElementById('row-fruits');
+var fruitNone = document.getElementById('fruits');
+
+var rowLegume= document.getElementById('row-legumes');
+var legumesNone = document.getElementById('legumes');
+
+var allNone = document.getElementById('all');
+
+
+/* Magasin complet */
+
+function displayNoneAll() {
+
+rowConfiture.style.display='block';
+rowBoisson.style.display='block';
+rowFruit.style.display='block';
+rowLegume.style.display='block';
+}
+
+allNone.addEventListener('click',displayNoneAll);
+
+
+/*Confitures*/
+
+function displayNoneConfiture() {
+
+rowConfiture.style.display='block';
+rowBoisson.style.display='none';
+rowFruit.style.display='none';
+rowLegume.style.display='none';
+}
+
+confitureNone.addEventListener('click',displayNoneConfiture);
+
+
+/*Boissons*/
+
+function displayNoneBoisson() {
+
+  rowBoisson.style.display='block';
+  rowConfiture.style.display='none';
+  rowFruit.style.display='none';
+  rowLegume.style.display='none';
+}
+
+boissonNone.addEventListener('click',displayNoneBoisson);
+
+
+/*Fruits*/
+
+function displayNoneFruit() {
+
+  rowFruit.style.display='block';
+  rowConfiture.style.display='none';
+  rowBoisson.style.display='none';
+  rowLegume.style.display='none';
+}
+
+fruitNone.addEventListener('click',displayNoneFruit);
+
+
+/*Légumes*/
+
+function displayNoneLegumes() {
+
+rowLegume.style.display='block';
+rowConfiture.style.display='none';
+rowBoisson.style.display='none';
+rowFruit.style.display='none';
+}
+
+legumesNone.addEventListener('click',displayNoneLegumes);
+
